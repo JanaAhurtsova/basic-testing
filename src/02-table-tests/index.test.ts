5192c8fbdea55cdec2a1c3fcedb887244bcb188a
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-25 21:49:04 +0300

    feat: add table test

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,33 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
     { a: 1, b: 2, action: Action.Add, expected: 3 },
     { a: 2, b: 2, action: Action.Add, expected: 4 },
     { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { a: 2, b: 1, action: Action.Substract, expected: 1 },
+    { a: 2, b: 2, action: Action.Substract, expected: 0 },
+    { a: 3, b: 2, action: Action.Substract, expected: 1 },
+    { a: 8, b: 2, action: Action.Divide, expected: 4 },
+    { a: 2, b: 2, action: Action.Divide, expected: 1 },
+    { a: 6, b: 2, action: Action.Divide, expected: 3 },
+    { a: 8, b: 2, action: Action.Multiply, expected: 16 },
+    { a: 2, b: 2, action: Action.Multiply, expected: 4 },
+    { a: 6, b: 2, action: Action.Multiply, expected: 12 },
+    { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
+    { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+    { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+    { a: 1, b: 2, action: '%', expected: null },
+    { a: 2, b: 3, action: '++', expected: null },
+    { a: 3, b: 2, action: '--', expected: null },
+    { a: '1', b: 2, action: Action.Add, expected: null },
+    { a: 2, b: true, action: Action.Add, expected: null },
+    { a: null, b: 2, action: Action.Add, expected: null },
+];
 
-describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+describe.only.each(testCases)('simpleCalculator', ({a, b, action, expected}) => {
+  test(`returns ${expected}`, () => {
+    const result = simpleCalculator({a, b, action});
+    expect(result).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
